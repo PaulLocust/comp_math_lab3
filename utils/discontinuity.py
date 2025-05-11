6fe461ab47aa5f9de7bf53aecfbd4d1222a7b4d2
Pavel Sarancha <sarancha.pasha@mail.ru>
2025-05-11 15:39:16 +0300

    fix discontinuity

--- utils/discontinuity.py
+++ utils/discontinuity.py
@@ -1,47 +1,49 @@
-MAX_BREAKPOINTS = 10_000  # Максимальное количество разрывов для поиска
+import math
+
+MAX_BREAKPOINTS = 10_000  # Максимальное количество допустимых разрывов
+
+
+def is_defined(func, x):
+    """
+    Проверяет, определена ли функция в точке x.
+    Возвращает False, если возникает исключение или результат невалидный.
+    """
+    try:
+        val = func(x)
+        if isinstance(val, complex):
+            return False
+        if math.isnan(val) or math.isinf(val):
+            return False
+        return True
+    except (ZeroDivisionError, ValueError, OverflowError, TypeError):
+        return False
 
 
 def get_discontinuity_points(func, a, b, n):
     """
-    Функция для поиска точек разрывов на интервале [a, b].
-    Если функция вызывает ошибку (например, деление на ноль), то эта точка считается точкой разрыва.
+    Ищет точки разрыва функции на отрезке [a, b] с разбиением на n частей.
 
-    :param func: функция, которую нужно проверить
-    :param a: нижний предел
-    :param b: верхний предел
-    :param n: количество разбиений интервала для проверки
-    :return: список точек разрыва
+    :param func: функция для анализа
+    :param a: начальный предел
+    :param b: конечный предел
+    :param n: количество разбиений
+    :return: список уникальных точек разрыва
     """
-    breakpoints = []
+    breakpoints = set()
 
-    # Проверка крайних точек интервала
-    try:
-        func(a)
-    except (ZeroDivisionError, OverflowError, ValueError):
-        breakpoints.append(a)
+    # Проверка крайних точек и центра
+    for point in [a, b, (a + b) / 2]:
+        if not is_defined(func, point):
+            breakpoints.add(round(point, 10))  # округляем для устранения шумов
 
-    try:
-        func(b)
-    except (ZeroDivisionError, OverflowError, ValueError):
-        breakpoints.append(b)
-
-    # Проверка середины интервала
-    try:
-        func((a + b) / 2)
-    except (ZeroDivisionError, OverflowError, ValueError):
-        breakpoints.append((a + b) / 2)
-
-    # Разбиение интервала на n частей и проверка каждой точки
+    # Проверка по сетке
     h = (b - a) / n
-    for i in range(n):
+    for i in range(n + 1):
         point = a + i * h
-        try:
-            func(point)
-        except (ZeroDivisionError, OverflowError, ValueError):
-            breakpoints.append(point)
+        if not is_defined(func, point):
+            breakpoints.add(round(point, 10))
             if len(breakpoints) >= MAX_BREAKPOINTS:
-                # Если слишком много разрывов, уменьшаем шаг и продолжаем поиск
-                return get_discontinuity_points(func, a, b, n // 10)
+                # При слишком большом числе разрывов уменьшаем точность
+                return get_discontinuity_points(func, a, b, max(n // 10, 10))
 
-    return list(set(breakpoints))  # Убираем дубликаты, если они есть
-
+    return sorted(breakpoints)
